de8f09cba6945648d8982d7b6f01eb5d1abb5e36
kazem198 <k_gh198@yahoo.com>
2023-10-09 23:28:54 +0330

    hand_distance and game added

---
+++ hand_distance.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+import math
+import cvzone
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+# x is the distance y is the value in cm
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+coff = np.polyfit(x, y, 2)  # AX^2 + BX + C
+
+detector = HandDetector(staticMode=False, maxHands=1,
+                        modelComplexity=1, detectionCon=0.8, minTrackCon=0.5)
+
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img, draw=False)
+
+    if hands:
+        # Information for the first hand detected
+        lmList1 = hands[0]["lmList"]  # Get the first hand detected
+        x, y, w, h = hands[0]["bbox"]
+        x1, y1 = lmList1[5][0:2]
+        x2, y2 = lmList1[17][0:2]
+        distance = int(math.sqrt((y2-y1)**2 + (x2-x1)**2))
+        print(distance)
+        A, B, C = coff
+        distanceCM = A*(distance)**2 + B*distance + C
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 12)
+        cvzone.putTextRect(img, f'{int(distanceCM)} cm', (x+5, y-10), 2)
+
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        cv2.destroyAllWindows()
+        break
+
+cap.release()
